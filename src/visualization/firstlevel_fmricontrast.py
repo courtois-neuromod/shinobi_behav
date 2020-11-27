1f3a968d0de03a4591e18c3c850dfa8003459431
Yann Harel <yharel109@gmail.com>
2020-11-27 11:53:07 -0500

    added fwhm smoothing

--- src/visualization/firstlevel_fmricontrast.py
+++ src/visualization/firstlevel_fmricontrast.py
@@ -46,7 +46,8 @@ fmri_glm = FirstLevelModel(t_r=1.49,
                            hrf_model='spm',
                            drift_model='cosine',
                            high_pass=.01,
-                           n_jobs=16)
+                           n_jobs=16,
+                           smoothing_fwhm=5)
 fmri_glm = fmri_glm.fit(fmri_img, allruns_events)
 
 # get stats map
@@ -60,7 +61,7 @@ uncorr_map, threshold = map_threshold(z_map, alpha=.001, height_control='fpr')
 # save images
 print('Generating views')
 view = plotting.view_img(clean_map, threshold=3, title='Left minus Right Hand (FDR=0.05), Noyaux > 10 voxels')
-view.save_as_html(figures_path + '/{}_LmR_statsmap_allruns_FDRcluster.html'.format(sub))
+view.save_as_html(figures_path + '/{}_LmR_statsmap_allruns_FDRcluster_fwhm5.html'.format(sub))
 
 view = plotting.view_img(uncorr_map, threshold=3, title='Left minus Right Hand (p<0.001), uncorr')
-view.save_as_html(figures_path + '/{}_LmR_statsmap_allruns_uncorr.html'.format(sub))
+view.save_as_html(figures_path + '/{}_LmR_statsmap_allruns_uncorr_fwhm5.html'.format(sub))
