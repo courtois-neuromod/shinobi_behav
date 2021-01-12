df43dd70aaf7311ca18aaa793523d37d2ce2b777
Yann Harel <yharel109@gmail.com>
2021-01-11 22:55:36 -0500

    added print to debug

--- src/visualization/firstlevel_fmricontrast.py
+++ src/visualization/firstlevel_fmricontrast.py
@@ -32,6 +32,7 @@ for ses in sorted(seslist):
     print('Processing {}'.format(ses))
     print(runs)
     for run in sorted(runs):
+        print('run : {}'.format(run))
         data_fname = dpath + 'derivatives/fmriprep-20.2lts/fmriprep/{}/{}/func/{}_{}_task-shinobi_run-{}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz'.format(sub, ses, sub, ses, run)
         confounds_fname = dpath + 'derivatives/fmriprep-20.2lts/fmriprep/{}/{}/func/{}_{}_task-shinobi_run-{}_desc-confounds_timeseries.tsv'.format(sub, ses, sub, ses, run)
         anat_fname = '/project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/data/anat/derivatives/fmriprep-20.2lts/fmriprep/{}/anat/{}_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz'.format(sub, sub)
@@ -61,7 +62,7 @@ for ses in sorted(seslist):
                                mask_img=anat_fname)
     fmri_glm = fmri_glm.fit(fmri_imgs, allruns_events, confounds=confounds)
     report = fmri_glm.generate_report(contrasts=['LeftH-RightH'])
-    report.save_as_html('/home/hyruuk/GitHub/neuromod/hyruuk_shinobi_behav/reports/{}_{}_LmR_flm.html'.format(sub, ses))
+    report.save_as_html(figures_path + '/{}_{}_LmR_flm.html'.format(sub, ses))
 
     # get stats map
     z_map = fmri_glm.compute_contrast(['LeftH-RightH'],
@@ -78,20 +79,3 @@ for ses in sorted(seslist):
     # save also uncorrected map
     view = plotting.view_img(uncorr_map, threshold=3, title='Left minus Right Hand (p<0.001), uncorr')
     view.save_as_html(figures_path + '/{}_{}_LmR_statsmap_allruns_uncorr_fwhm5.html'.format(sub, ses))
-
-'''
-# get stats map
-z_map = fmri_glm.compute_contrast(['LeftH-RightH'] * len(fmri_img),
-    output_type='z_score', stat_type='F')
-
-# compute thresholds
-clean_map, threshold = map_threshold(z_map, alpha=.05, height_control='fdr', cluster_threshold=10)
-uncorr_map, threshold = map_threshold(z_map, alpha=.001, height_control='fpr')
-
-# save images
-print('Generating views')
-view = plotting.view_img(clean_map, threshold=3, title='Left minus Right Hand (FDR=0.05), Noyaux > 10 voxels')
-view.save_as_html(figures_path + '/{}_LmR_statsmap_allruns_FDRcluster_fwhm5.html'.format(sub))
-
-
-'''
