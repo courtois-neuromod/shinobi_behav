3503b4390d881e1c2cdb8204aff20b547610b922
Yann Harel <yharel109@gmail.com>
2021-01-20 00:52:03 -0500

    manually removed the extra confounds based on shortest run

--- src/visualization/firstlevel_fmricontrast.py
+++ src/visualization/firstlevel_fmricontrast.py
@@ -79,7 +79,7 @@ for ses in sorted(seslist):
         design_matrix['LeftH'] = LeftH_ts_hpf_z
         design_matrix['RightH'] = RightH_ts_hpf_z
 
-        new_idx_con = 1
+        new_idx_con = 0
         for idx_con, con in enumerate(np.asarray(confounds[idx]).squeeze().T):
             if idx_con < conf_minlen-12 or idx_con >= confounds[idx].shape[1]-12:
                 design_matrix[idx_con] = con
