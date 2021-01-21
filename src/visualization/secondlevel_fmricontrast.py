de78ad29293289b9e3a561b43ee48950fe861b0c
Yann Harel <yharel109@gmail.com>
2021-01-21 16:19:43 -0500

    created secondlevel script

---
+++ src/visualization/secondlevel_fmricontrast.py
@@ -0,0 +1,136 @@
+import pandas as pd
+import os.path as op
+import matplotlib.pyplot as plt
+from src.params import figures_path, path_to_data
+from nilearn import plotting
+from nilearn import image
+import os
+import numpy as np
+from nilearn.plotting import plot_design_matrix
+from nistats.thresholding import map_threshold
+from nilearn.glm.first_level import FirstLevelModel
+from nilearn.input_data import NiftiMasker
+import load_confounds
+import pickle
+import nilearn
+from scipy import signal
+from scipy.stats import zscore
+
+
+
+
+
+ # Set constants
+sub = 'sub-01'
+actions = ['B', 'A', 'MODE', 'START', 'UP', 'DOWN', 'LEFT', 'RIGHT', 'C', 'Y', 'X', 'Z']
+dpath = path_to_data + 'shinobi/'
+
+
+
+seslist= os.listdir(dpath + sub)
+# load nifti imgs
+for ses in sorted(seslist):
+    runs = [filename[-13] for filename in os.listdir(dpath + '{}/{}/func'.format(sub, ses)) if 'bold.nii.gz' in filename]
+    fmri_imgs = []
+    design_matrices = []
+    confounds = []
+    confounds_cnames = []
+    print('Processing {}'.format(ses))
+    print(runs)
+    for run in sorted(runs):
+        print('run : {}'.format(run))
+        data_fname = dpath + 'derivatives/fmriprep-20.2lts/fmriprep/{}/{}/func/{}_{}_task-shinobi_run-{}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz'.format(sub, ses, sub, ses, run)
+        confounds_fname = dpath + 'derivatives/fmriprep-20.2lts/fmriprep/{}/{}/func/{}_{}_task-shinobi_run-{}_desc-confounds_timeseries.tsv'.format(sub, ses, sub, ses, run)
+        anat_fname = '/project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/data/anat/derivatives/fmriprep-20.2lts/fmriprep/{}/anat/{}_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz'.format(sub, sub)
+        fmri_img = image.concat_imgs(data_fname)
+        masker = NiftiMasker()
+        masker.fit(anat_fname)
+        confounds.append(pd.DataFrame.from_records(load_confounds.Params36().load(confounds_fname)))
+        fmri_imgs.append(fmri_img)
+        conf=load_confounds.Params36()
+        conf.load(confounds_fname)
+        confounds_cnames.append(conf.columns_)
+    # load events
+    with open(path_to_data + '{}_{}_events_files.pkl'.format(sub, ses), 'rb') as f:
+        allruns_events = pickle.load(f)
+
+
+    # find the shortest run and retain it's number of confounds
+    conf_lengths = []
+    for con in confounds:
+        conf_lengths.append(con.shape[1])
+    conf_minlen = np.min(conf_lengths)
+
+    # create design matrices
+    for idx, run in enumerate(sorted(runs)):
+        t_r = 1.49
+        n_slices = confounds[idx].shape[0]
+        frame_times = np.arange(n_slices) * t_r
+
+        design_matrix = nilearn.glm.first_level.make_first_level_design_matrix(frame_times,
+                                                                               events=allruns_events[idx],
+                                                                              drift_model=None,
+                                                                              add_regs=confounds[idx],
+                                                                              add_reg_names=confounds_cnames[idx])
+        LeftH_ts = np.asarray(design_matrix['LeftH'])
+        RightH_ts = np.asarray(design_matrix['RightH'])
+
+        b, a = signal.butter(3, 0.01, btype='high')
+        LeftH_ts_hpf = signal.filtfilt(b, a, LeftH_ts)
+        RightH_ts_hpf = signal.filtfilt(b, a, RightH_ts)
+        LeftH_ts_hpf_z = zscore(LeftH_ts_hpf)
+        RightH_ts_hpf_z = zscore(RightH_ts_hpf)
+
+        design_matrix['LeftH'] = LeftH_ts_hpf_z
+        design_matrix['RightH'] = RightH_ts_hpf_z
+
+        design_matrices.append(design_matrix)
+
+
+    cmaps = []
+    for idx, fmri_img in enumerate(fmri_imgs):
+        fmri_glm = FirstLevelModel(t_r=1.49,
+                                   noise_model='ar1',
+                                   standardize=False,
+                                   hrf_model='spm',
+                                   drift_model=None, # reactivate automatic drift compensation
+                                   high_pass=.01,
+                                   n_jobs=-1,
+                                   smoothing_fwhm=None,
+                                   mask_img=anat_fname)
+
+
+
+        fmri_glm = fmri_glm.fit(fmri_img, design_matrices=design_matrices[idx])
+
+        cmap = fmri_glm.compute_contrast(['LeftH-RightH'],
+                                          stat_type='F',
+                                          output_type='z_score')
+        cmaps.append(cmap)
+
+
+    second_level_input = cmaps
+    second_design_matrix = pd.DataFrame([1] * len(second_level_input),
+                                 columns=['intercept'])
+
+    from nilearn.glm.second_level import SecondLevelModel
+    second_level_model = SecondLevelModel(smoothing_fwhm=8.0)
+    second_level_model = second_level_model.fit(second_level_input,
+                                                design_matrix=second_design_matrix)
+
+    z_map = second_level_model.compute_contrast(output_type='z_score')
+
+    report = second_level_model.generate_report(contrasts=['intercept'])
+    report.save_as_html(figures_path + '/{}_{}_LmR_slm.html'.format(sub, ses))
+    
+    # compute thresholds
+    clean_map, threshold = map_threshold(z_map, alpha=.05, height_control='fdr', cluster_threshold=10)
+    uncorr_map, threshold = map_threshold(z_map, alpha=.001, height_control='fpr')
+
+    # save images
+    print('Generating views')
+    view = plotting.view_img(clean_map, threshold=3, title='Left minus Right Hand (FDR<0.05), Noyaux > 10 voxels')
+    view.save_as_html(figures_path + '/{}_{}_LmR_statsmap_allruns_FDRcluster_fwhm5.html'.format(sub, ses))
+    # save also uncorrected map
+    view = plotting.view_img(uncorr_map, threshold=3, title='Left minus Right Hand (p<0.001), uncorr')
+    view.save_as_html(figures_path + '/{}_{}_LmR_statsmap_allruns_uncorr_fwhm5.html'.format(sub, ses))
