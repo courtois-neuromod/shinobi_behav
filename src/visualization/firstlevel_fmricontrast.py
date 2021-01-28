70e11a26ed0a5f51f2e6bdb952c94c28985d26de
Yann Harel <yharel109@gmail.com>
2021-01-27 22:53:28 -0500

    removing the last drifts to get images -- SHOULD BE TEMPORARY

--- src/visualization/firstlevel_fmricontrast.py
+++ src/visualization/firstlevel_fmricontrast.py
@@ -80,8 +80,8 @@ for ses in sorted(seslist):
         design_matrix = nilearn.glm.first_level.make_first_level_design_matrix(frame_times,
                                                                                events=allruns_events[idx],
                                                                               drift_model=None,
-                                                                              add_regs=confounds[idx],
-                                                                              add_reg_names=confounds_cnames[idx])
+                                                                              add_regs=new_confounds,
+                                                                              add_reg_names=new_confounds_cnames)
         LeftH_ts = np.asarray(design_matrix['LeftH'])
         RightH_ts = np.asarray(design_matrix['RightH'])
 
