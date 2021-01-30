ecb634c14a0ba44385a5a58c754ac6d1f695290e
Hyruuk <yharel109@gmail.com>
2021-01-29 19:11:08 -0500

    adjusted slurm subm file

--- subm_firstlevel.sh
+++ subm_firstlevel.sh
@@ -2,10 +2,10 @@
 #SBATCH --account=rrg-pbellec
 #SBATCH --time=12:00:00
 #SBATCH --job-name=shinobi_firstlevel_fmricontrast
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 
 module load python/3.8.0
 
-/lustre03/project/6003287/hyruuk/.virtualenvs/hyruuk_shinobi_behav/bin/python /project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/src/visualization/firstlevel_fmricontrast.py
+/lustre03/project/6003287/hyruuk/.virtualenvs/hyruuk_shinobi_behav/bin/python /project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/src/visualization/firstlevel_allrunsnotrick_fmricontrast.py
