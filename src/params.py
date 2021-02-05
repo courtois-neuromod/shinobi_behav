f20ee5cef4522b3494d586d47d2a274f74034b72
Yann Harel <yharel109@gmail.com>
2021-02-04 20:44:49 -0500

    moved actions to general params

--- src/params.py
+++ src/params.py
@@ -2,3 +2,4 @@ subjects = ['sub-01', 'sub-02', 'sub-04', 'sub-06']
 levels = ['1', '4', '5']
 path_to_data = '/project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/data/'
 figures_path = '/project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/reports/figures/'
+actions = ['B', 'A', 'MODE', 'START', 'UP', 'DOWN', 'LEFT', 'RIGHT', 'C', 'Y', 'X', 'Z']
