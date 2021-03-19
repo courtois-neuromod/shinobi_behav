e4a8f6f18c087a00c8dd98ab619950c9eccd446d
Hyruuk <yharel109@gmail.com>
2021-03-19 12:05:22 -0400

    cleaned a bit and added mkdir

--- src/features/generate_annotations.py
+++ src/features/generate_annotations.py
@@ -1,20 +1,19 @@
-from src.params import actions, path_to_data
+from src.params import actions, path_to_data, subjects
 from src.data.data import retrieve_scanvariables
 from src.features.annotations import create_runevents
 import pickle
 import os
-dpath = '/media/hyruuk/Seagate Expansion Drive/DATA/shinobi/'
 import logging
 import pandas as pd
 
 
-subjects = ['sub-01']
-sessions = os.listdir(dpath + 'sub-01')
+dpath = '/media/hyruuk/Seagate Expansion Drive/DATA/shinobi/'
 
 
 
 def main():
     for sub in subjects:
+        sessions = os.listdir(dpath + sub)
         for ses in sorted(sessions):
             runs = [filename[-13] for filename in os.listdir(dpath + '{}/{}/func'.format(sub, ses)) if 'bold.nii.gz' in filename]
             for run in sorted(runs):
@@ -26,6 +25,8 @@ def main():
                 runvars = retrieve_scanvariables(files)
                 events_df = create_runevents(runvars, startevents, actions=actions)
                 events_path = path_to_data + 'processed/annotations/{}_{}_run-0{}.pkl'.format(sub, ses, run)
+                if not os.path.isdir(path_to_data + 'processed/annotations'):
+                    os.mkdir(path_to_data + 'processed/annotations')
                 with open(events_path, 'wb') as f:
                     pickle.dump(events_df, f)
 
