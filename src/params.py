ee2481c8f24454b59dc7caa32ae154bf99a2ac12
Hyruuk <yharel109@gmail.com>
2021-03-19 12:05:38 -0400

    updated params

--- src/params.py
+++ src/params.py
@@ -1,5 +1,5 @@
 subjects = ['sub-01', 'sub-02', 'sub-04', 'sub-06']
 levels = ['1', '4', '5']
-path_to_data = '/project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/data/'
-figures_path = '/project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/reports/figures/'
+path_to_data = '/home/hyruuk/GitHub/neuromod/hyruuk_shinobi_behav/data/'
+figures_path = '/home/hyruuk/GitHub/neuromod/hyruuk_shinobi_behav/reports/figures/'
 actions = ['B', 'A', 'MODE', 'START', 'UP', 'DOWN', 'LEFT', 'RIGHT', 'C', 'Y', 'X', 'Z']
