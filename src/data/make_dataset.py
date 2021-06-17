5cbaeb0bd278731716c0f4415351d22090ebb94e
Yann Harel <yharel109@gmail.com>
2021-06-17 12:44:39 -0400

    merged the two make_dataset files

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,23 +1,49 @@
 # -*- coding: utf-8 -*-
 import logging
 from src.params import *
-from data import combine_variables
-
+from data import combine_variables, remove_fake_reps
+import os.path as op
+import argparse
 
 def main():
     """ Runs data processing scripts to turn raw data from (../bids) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info('Generating data dict from raw data')
+
+    parser = argparse.ArgumentParser(description='Generates a dataset from raw .bk2 files.')
+    parser.add_argument('-n', '--nuc',
+                        action='store_true',
+                        dest='nuc',
+                        help='Use NUC files (behaviour only)'
+                        )
+    parser.add_argument('-s', '--scan',
+                        action='store_true',
+                        dest='scan',
+                        help='Use files related to scan sessions.'
+                        )
+
+    args = parser.parse_args()
+
 
     # start loop
     for subj in subjects:
         for level in levels:
-            print('Extracting game variables for {}_level-{}'.format(subj, level))
-            allvars = combine_variables(path_to_data, subj, level, save=False)
-            allvars = remove_fake_reps(allvars)
-            allvars_path = op.join(path_to_data, 'processed','{}_{}_allvars_scan.pkl'.format(subject, level))
+            if args.scan:
+                print('Extracting game variables (scans) for {}_level-{}'.format(subj, level))
+                allvars = combine_variables(path_to_data, subj, level, behav=False, save=False)
+                allvars = remove_fake_reps(allvars)
+                allvars_path = op.join(path_to_data, 'processed','{}_{}_allvars_scan.pkl'.format(subj, level))
+            elif args.nuc:
+                print('Extracting game variables (NUC) for {}_level-{}'.format(subj, level))
+                allvars = combine_variables(path_to_data, subj, level, behav=False, save=False)
+                allvars = remove_fake_reps(allvars)
+                allvars_path = op.join(path_to_data, 'processed','{}_{}_allvars_scan.pkl'.format(subj, level))
+            else:
+                print('Aborted.')
+                print('Please chose between --nuc and --scan arguments.')
+                quit()
             with open(allvars_path, 'wb') as f:
                 pickle.dump(allvars, f)
 
