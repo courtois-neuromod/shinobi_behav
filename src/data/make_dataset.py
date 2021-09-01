455cfe3ebff575f9c1e2deefdda74b7a7810044d
Hyruuk <yharel109@gmail.com>
2021-09-01 00:38:59 -0400

    added logs and progress bar

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 from src.params import *
-from data import combine_variables
+from data import combine_variables, remove_fake_reps
 
 
 def main():
@@ -9,25 +9,25 @@ def main():
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('Processing datasets for learning visualization.')
+    logger.info('Processing datasets for at-home VS in-scanner visualization.')
 
     # start loop
     for subj in subjects:
         for level in levels:
-            print('Extracting game variables for {}_level-{}'.format(subj, level))
-            print('Training sessions (NUC)')
+            logger.info('Extracting game variables for {}_level-{}'.format(subj, level))
+            logger.info('Training sessions (NUC)')
             allvars_behav = combine_variables(path_to_data, subj, level, behav=True, save=False)
             allvars_behav = remove_fake_reps(allvars_behav)
             allvars_behav_path = op.join(path_to_data, 'processed','{}_{}_allvars_behav.pkl'.format(subject, level))
             with open(allvars_scan_path, 'wb') as f:
                 pickle.dump(allvars_scan, f)
-            print('Scan sessions')
+            logger.info('Scan sessions')
             allvars_scan = combine_variables(path_to_data, subj, level, behav=False, save=False)
             allvars_scan = remove_fake_reps(allvars_behav)
             allvars_scan_path = op.join(path_to_data, 'processed','{}_{}_allvars_scan.pkl'.format(subject, level))
             with open(allvars_scan_path, 'wb') as f:
                 pickle.dump(allvars_scan, f)
-            print('Done.')
+            logger.info('Done.')
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
