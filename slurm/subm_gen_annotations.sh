7738d7ecb56f27205652ac25f7629df8cfdb58ce
Hyruuk <yharel109@gmail.com>
2021-09-27 15:00:53 -0400

    fixed path

--- slurm/subm_gen_annotations.sh
+++ slurm/subm_gen_annotations.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=rrg-pbellec
 #SBATCH --time=12:00:00
-#SBATCH --job-name=shinobi_firstlevel_fmricontrast
+#SBATCH --job-name=shinobi_annotations
 #SBATCH --mem=128G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 
 module load python/3.8.0
 
-/lustre03/project/6003287/hyruuk/.virtualenvs/hyruuk_shinobi_behav/bin/python /project/rrg-pbellec/hyruuk/hyruuk_shinobi_behav/src/features/generate_annotations.py
+/lustre03/project/6003287/hyruuk/.virtualenvs/hyruuk_shinobi_behav/bin/python /project/rrg-pbellec/hyruuk/shinobi_fmri/data/shinobi_behav/shinobi_behav/annotations/generate_annotations.py
