0c98a6100797e972a45e7593f912376b83e7a45b
Yann Harel <yharel109@gmail.com>
2022-01-31 12:59:08 -0500

    created slurm file for make dataset

---
+++ slurm/subm_make_dataset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=rrg-pbellec
+#SBATCH --time=12:00:00
+#SBATCH --job-name=shi_make_dataset
+#SBATCH --mem=128G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+
+module load python/3.8.0
+
+/home/hyruuk/python_envs/shinobi_env/bin/python /project/rrg-pbellec/hyruuk/shinobi_fmri/data/shinobi_behav/shinobi_behav/data/make_dataset.py
