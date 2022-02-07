39a4d97a7eab8a66988c5879fba28f571df6a683
Yann Harel <yharel109@gmail.com>
2022-02-07 18:23:32 -0500

    deprecated but cleaned compute_features

--- shinobi_behav/features/compute_features.py
+++ shinobi_behav/features/compute_features.py
@@ -7,24 +7,32 @@ import click
 
 
 def main():
-    """ Create averaged features based on the datadicts in ./data/processed/
+    """ CURRENTLY DEPRECATED
+    Create averaged features based on the datadicts in ./data/processed/
     These features are then used to plot the learning curves with src/visualization/generate_lcurves.py
     """
     path_to_data = shinobi_behav.DATA_PATH
     logger = logging.getLogger(__name__)
-    logger.info('Create repetition-wise features')
+    logger.info("Create repetition-wise features")
 
-    setup='home'
+    setup = "home"
     # start loop
     for subj in shinobi_behav.SUBJECTS:
         for level in shinobi_behav.LEVELS:
-            logger.info('Extracting game variables for {}_level-{}'.format(subj, level))
-            allvars = get_levelreps(path_to_data, subj, level, remove_fake_reps=True, setup=setup)
-            load_features_dict(path_to_data, subj, level, save=True, metric='mean', allvars=allvars)
-            load_features_dict(path_to_data, subj, level, save=True, metric=None, allvars=allvars)
+            logger.info(f"Extracting game variables for {subj}_level-{level}")
+            allvars = get_levelreps(
+                path_to_data, subj, level, remove_fake_reps=True, setup=setup
+            )
+            load_features_dict(
+                path_to_data, subj, level, save=True, metric="mean", allvars=allvars
+            )
+            load_features_dict(
+                path_to_data, subj, level, save=True, metric=None, allvars=allvars
+            )
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     main()
