a990dd6d614c61d57efb5d3befb404820c814625
Yann Harel <yharel109@gmail.com>
2022-02-07 17:45:49 -0500

    updated get_dataset file

--- shinobi_behav/data/get_dataset.sh
+++ shinobi_behav/data/get_dataset.sh
@@ -1,14 +1,14 @@
 # Run these lines to install both shinobi and shinobi_beh datasets
 cd ./data
-export AWS_ACCESS_KEY_ID=yharel  AWS_SECRET_ACCESS_KEY=qrVwnh1NYZfpCHtTL7glEfdY
-datalad install git@github.com:courtois-neuromod/shinobi
+git clone git@github.com:courtois-neuromod/shinobi_training
+datalad get ./shinobi_training
+git clone git@github.com:courtois-neuromod/shinobi
 cd shinobi
 git checkout event_files
-datalad get ./
-scp -r yharel@elm.criugm.qc.ca:/data/neuromod/DATA/games/shinobi_beh ./
+datalad get .
+cd ..
+git clone git@github.com:courtois-neuromod/shinobi.stimuli
+datalad get ./shinobi.stimuli
 
 # Additional lines to setup gym environment
-cd stimuli
-datalad get ./
-conda activate hyruuk_shinobi_behav
-python3 -m retro.import ./data/shinobi/stimuli/ShinobiIIIReturnOfTheNinjaMaster-Genesis
+python3 -m retro.import ./data/shinobi.stimuli/ShinobiIIIReturnOfTheNinjaMaster-Genesis
