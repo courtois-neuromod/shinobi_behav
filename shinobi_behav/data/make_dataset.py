b8c957e78c82f63198997b54ab2fe8e40de428fc
Yann Harel <yharel109@gmail.com>
2022-02-07 17:58:58 -0500

    cleaned script

--- shinobi_behav/data/make_dataset.py
+++ shinobi_behav/data/make_dataset.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import logging
 import shinobi_behav
 from shinobi_behav.data.data import get_levelreps
@@ -9,52 +8,47 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "-s",
-    "--subject",
-    default='04',
-    type=str,
-    help="Subject to process",
+    "-s", "--subject", default="01", type=str, help="Subject to process",
 )
-parser.add_argument(
-	"-l",
-	"--level",
-	default = '1-0',
-	type = str,
-	help="Level to process"
-)
+parser.add_argument("-l", "--level", default="1", type=str, help="Level to process")
 
 args = parser.parse_args()
 
+
 def main():
-    """ Extracts variables from bk2 files and stores them as lists of dictionnaries,
-    splitted by subject and by level, and sorted by date. This step is a prerequisite for
-    features computation intended for the analysis of learning progression.
+    """ Extracts variables from bk2 files and stores them as lists of
+    dictionnaries, splitted by subject, setup and level, and sorted by date.
+    This step is a prerequisite for features computation.
     """
     path_to_data = shinobi_behav.DATA_PATH
-    subj = 'sub-' + args.subject
+    subj = "sub-" + args.subject
     level = args.level
     logger = logging.getLogger(__name__)
-    logger.info('Processing datasets for at-home VS in-scanner analysis.')
-    if not op.isdir(op.join(path_to_data, 'processed')):
-        os.mkdir(op.join(path_to_data, 'processed'))
-        logger.info('Directory created')
+    logger.info("Processing datasets for at-home VS in-scanner analysis.")
+    if not op.isdir(op.join(path_to_data, "processed")):
+        os.mkdir(op.join(path_to_data, "processed"))
+        logger.info("Directory created")
     else:
-        logger.info('Directory already exists')
-
-#    for subj in shinobi_behav.SUBJECTS:
-#        for level in shinobi_behav.LEVELS:
-    for setup in ['scan', 'home']:
-        level_variables_path = op.join(path_to_data, 'processed','{}_{}_allvars_{}.pkl'.format(subj, level, setup))
+        logger.info("Directory already exists")
+    # Start loop
+    for setup in ["scan", "home"]:
+        level_variables_path = op.join(
+            path_to_data, "processed", f"{subj}_{level}_allvars_{setup}.pkl"
+        )
         if not os.path.exists(level_variables_path):
-            logger.info('Extracting game variables for {}_level-{}'.format(subj, level))
-            logger.info('Training sessions ({})'.format(setup))
-            level_variable = get_levelreps(path_to_data, subj, level, remove_fake_reps=True, setup=setup)
-            with open(level_variables_path, 'wb') as f:
+            logger.info(f"Extracting game variables for {subj}_level-{level}")
+            logger.info("Training sessions ({})".format(setup))
+            # Extract variables and store them by sub*level*setup
+            level_variables = get_levelreps(
+                path_to_data, subj, level, remove_fake_reps=True, setup=setup
+            )
+
+            with open(level_variables_path, "wb") as f:
                 pickle.dump(level_variables, f)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     main()
