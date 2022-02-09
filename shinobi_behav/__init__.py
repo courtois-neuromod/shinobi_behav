ac6bd7189de6211ec1dd472cffcbf9cced224c45
Hyruuk <yharel109@gmail.com>
2022-02-09 16:10:06 -0500

    added GAME_FS variable

--- shinobi_behav/__init__.py
+++ shinobi_behav/__init__.py
@@ -3,6 +3,7 @@ import os.path as op
 SUBJECTS = ['sub-01', 'sub-02', 'sub-04', 'sub-06']
 LEVELS = ['1', '4', '5']
 ACTIONS = ['B', 'A', 'MODE', 'START', 'UP', 'DOWN', 'LEFT', 'RIGHT', 'C', 'Y', 'X', 'Z']
+GAME_FS = 60
 
 SRC_PATH = op.join('.')
 DATA_PATH = op.join(SRC_PATH, 'data')
