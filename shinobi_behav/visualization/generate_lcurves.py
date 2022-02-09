6b4e423e9d0026fb73e9b5778c85475435b20b40
Yann Harel <yharel109@gmail.com>
2022-02-09 07:55:48 -0500

    idk

---
+++ shinobi_behav/visualization/generate_lcurves.py
@@ -0,0 +1,12 @@
+import logging
+import shinobi_behav
+
+def main():
+
+
+if __name__ == '__main__':
+
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    main()
