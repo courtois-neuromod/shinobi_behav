939c68392b0d732b6d37a9302285ea4d5d894ef4
Yann Harel <yharel109@gmail.com>
2022-02-14 14:06:28 -0500

    created parsing

---
+++ shinobi_behav/parsing.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-s", "--subject", default="01", type=str, help="Subject to process",
+)
+
+parser.add_argument(
+    "-l", "--level", default="1", type=str, help="Level to process",
+)
